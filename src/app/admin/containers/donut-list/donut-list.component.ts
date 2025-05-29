47a3fde8e6cbbaf9e09fcb53876d9ffaed43cd9c
Josue <josgar_14@hotmail.com>
2025-05-29 02:41:25 -0400

    adding donut-card component

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -5,44 +5,39 @@ import { Donut } from '../../models/donut.model';
   selector: 'app-donut-list',
   template: `
     <div>
-      <div>
-        {{donut.name}}
-        {{donut.price}}
-      </div>
+      <app-donut-card [donut]="donuts[0]"></app-donut-card>
+      <app-donut-card [donut]="donuts[1]"></app-donut-card>
+      <app-donut-card [donut]="donuts[2]"></app-donut-card>
     </div>
   `,
-  styles: [
-  ]
+  styles: [],
 })
 export class DonutListComponent {
-  donut!: Donut;
   donuts!: Donut[];
 
   ngOnInit(): void {
-
     this.donuts = [
       {
         id: '5545',
         name: 'Just Chocolate',
         icon: 'just-chocolate',
         price: 199,
-        description: 'For the pure chocoholic'
+        description: 'For the pure chocoholic',
       },
       {
         id: '23333',
         name: 'Glazed Fudge',
         icon: 'glazed-fudge',
         price: 129,
-        description: 'Sticky perfection'
+        description: 'Sticky perfection',
       },
       {
         id: '1234',
         name: 'Caramel Swirl',
         icon: 'caramel-swirl',
         price: 139,
-        description: 'Chocolate drizzled with caramel'
-      }
+        description: 'Chocolate drizzled with caramel',
+      },
     ];
-    this.donut = this.donuts[0];
   }
 }
