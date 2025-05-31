db5635d7ea57203a42b7d37f31ee69792da10f7d
Josue <josgar_14@hotmail.com>
2025-05-31 01:32:54 -0400

    adding styles 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: ` <div class="app">
+    <header class="header">
+      <img class="logo" src="/assets/img/logo.svg" alt="Ultimate Donuts">
+    </header>
     <app-donut-list></app-donut-list>
   </div>
   `,
   styles: [
     `
       .app {
-        margin-top: 50px;
-        font-size: 22px;
-        color: #fff;
-        text-align: center;
+        background: #fff;
+        border-radius: 8px;
+        max-width: 400px;
+        width: 94%;
+        margin: 25px auto;
+        padding: 25px;
+        border: 4px solid #ef9fc
+      }
+      .header {
+        display: flex;
+        justify-content: center;
+        margin-bottom: 25px;
+      }
+      .logo{
+        width: 100px;
+        height: 88px;
+        
       }
     `,
   ],
