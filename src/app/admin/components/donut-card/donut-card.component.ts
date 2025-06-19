7637c717323ef7eb51eb2aaf231aa30f84bd169a
Josue <jgarcia@neural.com.do>
2025-06-18 23:24:35 -0400

    Update donut-card.component.ts
    ngSwitchCase with templates

--- src/app/admin/components/donut-card/donut-card.component.ts
+++ src/app/admin/components/donut-card/donut-card.component.ts
@@ -12,9 +12,12 @@ import { Donut } from '../../models/donut.model';
       <p class="donut-card-name">
         {{ donut.name }}
         <ng-container [ngSwitch]="donut.promo">
-          <span *ngSwitchCase="'new'" class="donut-card-label">NEW</span>
-          <span *ngSwitchCase="'limited'" class="donut-card-label">LIMITED</span>
-          <span *ngSwitchDefault class="donut-card-label">Nothing special...</span>
+          <span class="donut-card-label">
+            <ng-template [ngSwitchCase]="'new'">New</ng-template>
+            <ng-template [ngSwitchCase]="'limited'">Limited</ng-template>
+            <ng-template [ngSwitchDefault]>Nothing special...</ng-template>
+
+          </span>
         </ng-container>
       </p>
       <p class="donut-card-price">
