c04a763c9bd4123566747f8652375ee99002498e
Josue <jgarcia@neural.com.do>
2025-07-06 02:29:27 -0400

    configure and proxy a local http server db.json proxy.conf.json

--- src/app/admin/services/donut.service.ts
+++ src/app/admin/services/donut.service.ts
@@ -6,43 +6,7 @@ import { Donut } from '../models/donut.model';
 })
 export class DonutService {
 
-  private donuts: Donut[] = [
-    {
-      id: '5545',
-      name: 'Just Chocolate',
-      icon: 'just-chocolate',
-      price: 199,
-      promo: 'limited',
-      description: 'For the pure chocoholic',
-    },
-    {
-      id: '23333',
-      name: 'Glazed Fudge',
-      icon: 'glazed-fudge',
-      price: 129,
-      promo: 'new',
-      description: 'Sticky perfection',
-    },
-    {
-      id: '1234',
-      name: 'Caramel Swirl',
-      icon: 'caramel-swirl',
-      price: 139,
-      description: 'Chocolate drizzled with caramel',
-    }, {
-      id: '23424',
-      name: 'Sour Supreme',
-      icon: 'sour-supreme',
-      price: 233,
-      description: 'For the sour advocate.',
-    }, {
-      id: 'fdfsdf',
-      name: 'Zesty Lemon',
-      icon: 'zesty-lemon',
-      price: 123,
-      description: 'Delicious lucios lemon.',
-    },
-  ];
+  private donuts: Donut[] = [];
 
   constructor() { }
 
