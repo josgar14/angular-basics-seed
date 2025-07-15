b2ede75260d0ead3602b63bf7b29975a7ce56fc0
Josue <jgarcia@neural.com.do>
2025-07-15 05:47:24 -0400

    error handling

--- src/app/admin/containers/donut-single/donut-single.component.ts
+++ src/app/admin/containers/donut-single/donut-single.component.ts
@@ -6,30 +6,45 @@ import { DonutService } from '../../services/donut.service';
   selector: 'app-donut-single',
   template: `
     <div>
-      <app-donut-form [donut]="donut" (create)="onCreate($event)" (update)="onUpdate($event)" (delete)="onDelete($event)"></app-donut-form>
+      <app-donut-form
+        [donut]="donut"
+        (create)="onCreate($event)"
+        (update)="onUpdate($event)"
+        (delete)="onDelete($event)"
+      ></app-donut-form>
     </div>
   `,
-  styles: [
-  ]
+  styles: [],
 })
 export class DonutSingleComponent {
   donut!: Donut;
 
-  constructor(private donutService: DonutService) { }
+  constructor(private donutService: DonutService) {}
 
   ngOnInit(): void {
-    this.donutService.readOne('BDQLQKf').subscribe((donut: Donut) => this.donut = donut);
+    this.donutService
+      .readOne('BDQLQKf')
+      .subscribe((donut: Donut) => (this.donut = donut));
   }
 
   onCreate(donut: Donut) {
-    this.donutService.create(donut).subscribe(() => console.log('Created successfully!'));
+    this.donutService
+      .create(donut)
+      .subscribe(() => console.log('Created successfully!'));
   }
 
   onUpdate(donut: Donut) {
-    this.donutService.update(donut).subscribe(() => console.log("Updated successfully"));
+    this.donutService
+      .update(donut)
+      .subscribe({
+        next: () => console.log('Updated successfully'),
+        error: (err) => console.log('onUpdate error: ', err),
+      });
   }
 
   onDelete(donut: Donut) {
-    this.donutService.delete(donut).subscribe(() => console.log("Deleted successfully"));
+    this.donutService
+      .delete(donut)
+      .subscribe(() => console.log('Deleted successfully'));
   }
 }
