6edb164c7a9cd0c613767adc3d3986442646382c
Josue <jgarcia@neural.com.do>
2025-07-27 23:54:47 -0400

    programmatic navigation with router navigate

--- src/app/admin/containers/donut-single/donut-single.component.ts
+++ src/app/admin/containers/donut-single/donut-single.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Donut } from '../../models/donut.model';
 import { DonutService } from '../../services/donut.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-donut-single',
@@ -24,29 +24,30 @@ export class DonutSingleComponent {
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private donutService: DonutService
   ) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     console.log(id);
-    
+
     this.donutService
       .readOne(id)
       .subscribe((donut: Donut) => (this.donut = donut));
-    
+
     this.isEdit = this.route.snapshot.data['isEdit'];
   }
 
   onCreate(donut: Donut) {
     this.donutService
       .create(donut)
-      .subscribe(() => console.log('Created successfully!'));
+      .subscribe((donut) => this.router.navigate(['admin', 'donuts', donut.id]));
   }
 
   onUpdate(donut: Donut) {
     this.donutService.update(donut).subscribe({
-      next: () => console.log('Updated successfully'),
+      next: () => this.router.navigate(['admin']),
       error: (err) => console.log('onUpdate error: ', err),
     });
   }
@@ -54,6 +55,6 @@ export class DonutSingleComponent {
   onDelete(donut: Donut) {
     this.donutService
       .delete(donut)
-      .subscribe(() => console.log('Deleted successfully'));
+      .subscribe(() => this.router.navigate(['admin']));
   }
 }
